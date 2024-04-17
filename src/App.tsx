5c333eb46dfc6cb762de6fb0374863b7f6f8c9da
Khayle <MKhayle@users.noreply.github.com>
2024-04-17 23:34:56 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -87,9 +87,9 @@ function App() {
 
         <div className="subtle">
           Board ID: {boardId} |{" "}
-          <a href="https://github.com/tmyt/idea-elemental">
-            Version: {__COMMIT_HASH__}
+          <a href="https://github.com/tmyt/idea-elemental">Forked from tmyt/idea-elemental
           </a>
+            Version: {__COMMIT_HASH__}
         </div>
       </div>
     </>
